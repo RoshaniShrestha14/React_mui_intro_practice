7f4064fea708017d2d6e2852752e7de2364105b0
Roshani2211205 <roshani2211205@iimscollege.edu.np>
2025-01-30 12:11:18 +0545

    first commit

---
+++ mui-intro/src/App.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import { Typography, Box, Button } from "@mui/material";
+
+const App = () => {
+  const [count, setCount] = useState(0); // Initialize state
+
+  return (
+    <Box>
+      <Typography variant="h3">{count}</Typography>
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={() => {
+          const newCount = count + 1; // Increment count
+          setCount(newCount);
+        }}
+        style={{ marginRight: "8px" }} // Add some spacing between buttons
+      >
+        Increase count
+      </Button>
+      <Button
+        variant="contained"
+        color="secondary"
+        onClick={() => {
+          const newCount = count - 1; // Decrement count
+          setCount(newCount);
+        }}
+      >
+        Decrease count
+      </Button>
+    </Box>
+  );
+};
+
+export default App;
---
+++ mui-intro/src/App1.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Typography } from "@mui/material";
+import { Box } from '@mui/material';
+
+const App1 = () => {
+  return (
+    <Box>
+      <Typography variant="h1">Heading 1</Typography>
+      <Typography variant="h2">Heading 2</Typography>
+      <Typography variant="h3">Heading 3</Typography>
+      <Typography variant="h4">Heading 4</Typography>
+      <Typography variant="h5">Heading 5</Typography>
+    </Box>
+  );
+}
+
+export default App1
